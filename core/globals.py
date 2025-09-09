187479320c948ca9ebb908c622bd3b4a4a6a2d22
didi24391 <49967194+didi24391@users.noreply.github.com>
2025-09-09 18:40:05 +0800

    Update globals.py

--- core/globals.py
+++ core/globals.py
@@ -1,4 +1,13 @@
 import onnxruntime
+import torch
 
-use_gpu = False
-providers = onnxruntime.get_available_providers()
+use_gpu = True
+available_providers = onnxruntime.get_available_providers()
+
+if use_gpu and 'CUDAExecutionProvider' in available_providers and torch.cuda.is_available():
+    providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
+else:
+    use_gpu = False
+    providers = ['CPUExecutionProvider']
+
+print(f"[INFO] Using GPU: {use_gpu}, ONNXRuntime providers: {providers}")
